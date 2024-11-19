935be81ad4f6b500c6912e1840e6d57afe79f37d
ttigarmilioner <163966398+ttigarmilioner@users.noreply.github.com>
2024-11-20 01:45:35 +1000

    add commit

--- BankHotel/js/main.js
+++ BankHotel/js/main.js
@@ -3,6 +3,26 @@
 (function () {
 
 
+    //Бургер-меню
+    document.addEventListener('click', burgerInit)
+
+    function burgerInit(e) {
+
+        const burgerIcon = e.target.closest('.burger__icon')
+        const burgerNavLink = e.target.closest('.nav__link')
+
+        if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 1365) return
+
+        if (!document.body.classList.contains('body--opened-menu')) {
+            document.body.classList.add('body--opened-menu')
+        } else {
+            document.body.classList.remove('body--opened-menu')
+        }
+
+    }
+
+
     // Свайпер
     const swiper = new Swiper('.swiper', {
         loop: true,
